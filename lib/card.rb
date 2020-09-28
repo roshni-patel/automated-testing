9f9b7edbb557bcb053c79fae972dbee46d0849ec
roshni-patel <rpatel01@wesleyan.edu>
2020-09-28 10:36:05 -0500

    Wave 2 Complete

--- lib/card.rb
+++ lib/card.rb
@@ -1,17 +1,34 @@
 # card.rb
+VALID_SUITS = [:hearts, :spades, :clubs, :diamonds]
 
 class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
-    raise ArgumentError.new("Invalid suit.") unless [:hearts, :spades, :clubs, :diamonds].include? suit
+    raise ArgumentError.new("Invalid suit.") unless VALID_SUITS.include? suit
     raise ArgumentError.new("Invalid value.") if value < 1 || value > 13
 
     @value = value
     @suit = suit
   end
 
+  def face_card(value)
+    case value
+    when "Ace"
+      1
+    when "Jack"
+      11
+    when "Queen"
+      12
+    when "King"
+      13
+    else
+      value
+    end
+    return value
+  end
+
   def to_s
-    return "#{value} of #{suit.to_s}"
+    return "#{face_card(value)} of #{suit.to_s}"
   end
 end
--- test/card_test.rb
+++ test/card_test.rb
@@ -27,6 +27,17 @@ describe Card do
     it "to_s returns a readable String value logically for values 2-10" do
       # Test to ensure that to_s works for cards values 2-10
       # for example:  "2 of diamonds"
+      card_1 = Card.new(3, :hearts)
+      expect(card_1.to_s).must_equal "3 of hearts"
+
+      card_2 = Card.new(2, :diamonds)
+      expect(card_2.to_s).must_equal "2 of diamonds"
+
+      card_3 = Card.new(4, :spades)
+      expect(card_3.to_s).must_equal "4 of spades"
+
+      card_4 = Card.new(5, :clubs)
+      expect(card_4.to_s).must_equal "5 of clubs"
     end
 
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
@@ -41,6 +52,18 @@ describe Card do
       #  11: Jack
       #  12: Queen
       #  13: King
+
+      face_cards = {
+          1 => "Ace",
+          11 => "Jack",
+          12 => "Queen",
+          13 => "King"
+       }
+
+      face_cards.each do |value, face_card|
+        card = Card.new(value, :hearts)
+        expect(card.to_s).must_equal "#{value} of hearts"
+      end
     end
   end
 
@@ -48,12 +71,32 @@ describe Card do
 
     it "Can retrieve the value of the card using a `.value`." do
       # ensure that `.value works as expected`
+      card_1 = Card.new(3, :hearts)
+      expect(card_1.value).must_equal 3
+
+      card_2 = Card.new(2, :diamonds)
+      expect(card_2.value).must_equal 2
+
+      card_3 = Card.new(4, :spades)
+      expect(card_3.value).must_equal 4
+
+      card_4 = Card.new(5, :clubs)
+      expect(card_4.value).must_equal 5
     end
 
     it "Can retrieve the value of the card using a `.suit`." do
       # ensure that `.suit works as expected returning the symbol of the suit`
+      card_1 = Card.new(3, :hearts)
+      expect(card_1.suit).must_equal :hearts
 
+      card_2 = Card.new(2, :diamonds)
+      expect(card_2.suit).must_equal :diamonds
+
+      card_3 = Card.new(4, :spades)
+      expect(card_3.suit).must_equal :spades
+
+      card_4 = Card.new(5, :clubs)
+      expect(card_4.suit).must_equal :clubs
     end
   end
-
 end
