be8757b9826a8248f53182dc8d2e005157d3df8f
roshni-patel <rpatel01@wesleyan.edu>
2020-09-28 22:44:27 -0500

    Wave 3 Complete

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,19 +1,27 @@
-
 # deck.rb
-
 require_relative 'card'
 
 class Deck
+  attr_reader :cards
 
   def initialize
-
+    @cards = []
+    [:hearts, :spades, :clubs, :diamonds].each do |suit|
+      (1..13).each do |value|
+        @cards << Card.new(value, suit)
+      end
+    end
   end
 
   def draw
-    # returns a card
+    return @cards.pop
   end
 
   def shuffle
-    # shuffles the deck
+    return @cards.shuffle
+  end
+
+  def count
+    return @cards.length
   end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,9 +1,50 @@
 require_relative 'test_helper'
 
 describe Deck do
-  # Write tests for the Deck class.
-  # What should you test?
-    # 1.  Expected Behavior
-    # 2.  Edge cases
+  before do
+    @deck = Deck.new
+  end
 
+  describe "Constructor method" do
+    it "can be instantiated" do
+      expect(@deck).must_be_instance_of Deck
+    end
+
+    it "has 52 cards in a deck" do
+      expect(@deck.cards.length).must_equal 52
+    end
+  end
+
+  describe "Draw method" do
+    it "can be called" do
+      expect(@deck).must_respond_to :draw
+    end
+
+    it "can draw a card from the deck" do
+      expect(@deck.draw).must_be_instance_of Card
+      expect(@deck.cards.length).must_equal 51
+    end
+
+    it "can return a deck of 52 cards" do
+      52.times do
+        expect(@deck.draw).must_be_instance_of Card
+      end
+    end
+  end
+
+  describe "Shuffle method" do
+    it "can be called" do
+      expect(@deck).must_respond_to :shuffle
+    end
+  end
+
+  describe "Count method" do
+    it "can return the number of cards in the deck" do
+      19.times do
+        @deck.draw
+      end
+
+      expect(@deck.count).must_equal 33
+    end
+  end
 end
